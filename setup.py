1f09d4afcb7203eb55c1c62b1121a822a6fa7c2f
Adam Groszer <agroszer@mail.com>
2010-08-30 08:52:48 +0000

    Back to development: 0.5.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.json',
-    version='0.5.4',
+    version='0.5.5dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Zope3 JSON base libraries",
