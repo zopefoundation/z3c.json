746bfcf2df77613687a6d010776b44262d179206
Marius Gedminas <marius@gedmin.as>
2013-02-26 12:01:33 +0000

    Back to development: 0.5.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.json',
-    version='0.5.5',
+    version='0.5.6.dev0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Zope3 JSON base libraries",
